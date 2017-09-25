046a83b3387d42da088194b5794728095a757a2b
kgb <kgb@KGB.localdomain>
2017-09-25 14:32:28 -0500

    Finished 09-25-2017 activites

---
+++ SI Sessions/09-25-2017 - Chapter 3 - CMATH - Formatting Output/Problem1.cpp
@@ -0,0 +1,38 @@
+/*
+1.) Celsius to Fahrenheit
+    Prompt User to enter the temperature as Celsius and then
+    convert Celsius temperatures to Fahrenheit temperatures. The
+    formula is:
+
+    F = (9/5)C + 32
+    F is the Fahrenheit temperature, and C is the Celsius temperature.
+*/
+
+// Headers
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// Global variables
+
+// Function declarations
+
+int main()
+{
+	//	Your code goes here
+
+
+	//	Make sure we place the end message on a new line
+    cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+    system("PAUSE");
+
+	/*
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+    return 0;
+}
--- SI Sessions/09-25-2017 - Chapter 3 - CMATH - Formatting Output/interestEarnedProblem.cpp
+++ SI Sessions/09-25-2017 - Chapter 3 - CMATH - Formatting Output/interestEarnedProblem.cpp
@@ -3,17 +3,15 @@
   to give you the amount in your savings.
 */
 #include <iostream>
-#include <iomanip>
-#include <cmath>
+
+
+
 using namespace std;
 
-int main()
+double main()
 {
-  int timesCompounded = 0;
-  double interestRate = 0.0,
-         principal = 0.0,
-         interest = 0.0,
-         amount = 0.0;
+  int timesCompounded = 0.0;
+  double interestRate = 0.0, interest = 0.0, amount = 0.0;
 
   // Output Formatting
   cout << endl << setprecision(2) << fixed << showpoint;
@@ -28,16 +26,13 @@ int main()
 
   // Formulas
   amount = principal * pow((1 + ((interestRate/100.0) / timesCompounded)), timesCompounded);
-  interest = amount - principal;
 
   // Output to User
   cout << "\n==================================================\n";
   cout << "Interest Rate:     " << setw(15) << interestRate << "%" << endl;
   cout << "Times Compounded:  " << setw(14) << timesCompounded << endl;
   cout << "Principal:         " << setw(10) << "$ " << principal << endl;
-  cout << "Interest:          " << setw(10) << "$ " << interest << endl;
   cout << "Amount in Savings: " << setw(10) << "$ " << amount << endl;
 
-  //system("PAUSE");
-  return 0;
+  return 0.0;
 }
