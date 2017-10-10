5bc953862aa9272a20ee3d2fc92e60ed172b60cc
nabler <mohamed.rahaman@my.tccd.edu>
2017-10-10 00:50:08 -0500

    corrected errors

--- SI Sessions/10-04-2017 and 10-05-2017 - Chapter 4 - Selection Statements/selectionErrorSolution.cpp
+++ SI Sessions/10-04-2017 and 10-05-2017 - Chapter 4 - Selection Statements/selectionErrorSolution.cpp
@@ -21,43 +21,11 @@ int main()
   else
   {
     quotient = num1 / num2;
-    cout << "The quotient of " << num1 <<
-    cout << " divided by " << num2 << " is ";
+    cout << "The quotient of " << num1
+         << " divided by " << num2 << " is ";
     cout << quotient << endl;
   }
 
   system("PAUSE");
   return 0;
 }
-
-/*
-#include <i0stream> // 8
-using namespace std;
-
-int main()
-{
-  int num1, num2, quotient; // 1
-
-  cout << "Enter a number:" << end1; // 2,3
-  cin >> num1;
-  cout << "Enter another number: ";
-  cin >> num2;
-
-  if (num2 == 0)
-  // 4
-    cout << "Division by zero is not possible.\n";
-    cout << "Please run the program again ";
-    cout << "and enter a number besides zero.\n";
-  // 4
-  else () // 5
-  // 6
-    quotient = num1 / num2;
-    cout << "The quotient of " << num1 <<
-    cout << " divided by " << num2 << " is ";
-    cout << quotient << endl;
-  // 6
-
-  // 7
-  return 0.0; // 9
-}
-*/
