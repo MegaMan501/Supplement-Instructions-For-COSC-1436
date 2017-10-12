1e82e94b3978b561faf0348ce4ab244f6df40c19
kgb <kgb@KGB.localdomain>
2017-10-11 23:12:04 -0500

    Errors solved

--- Resources/Temp/switchChoiceSolution.cpp
+++ Resources/Temp/switchChoiceSolution.cpp
@@ -22,12 +22,10 @@ int main()
       break;
     case 'c':
     case 'C': cout << cout << "You entered C.\n";
-<<<<<<< HEAD
-=======
       break;
     default:
-      cout << "That is an invalid choice.\n";
->>>>>>> d4bf9e7aa002ca600a4d2827b20c1b74be8ff6ad
+      cout << "Error You have entered the wrong character.\n"
+          << "Restart the program.";
       break;
   }
 
