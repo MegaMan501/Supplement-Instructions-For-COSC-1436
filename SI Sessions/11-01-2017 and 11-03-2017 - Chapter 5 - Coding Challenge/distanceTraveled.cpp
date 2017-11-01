f8444947f744cf40202346a96eb3d3ea477c3bd2
MegaMan501 <31483985+MegaMan501@users.noreply.github.com>
2017-11-01 15:08:38 -0500

    Finalized the activity for 11/01 and 11/02.

--- SI Sessions/11-01-2017 and 11-03-2017 - Chapter 5 - Coding Challenge/distanceTraveled.cpp
+++ SI Sessions/11-01-2017 and 11-03-2017 - Chapter 5 - Coding Challenge/distanceTraveled.cpp
@@ -1,30 +1,3 @@
-/*******************************************************************************************************
-    Distance Traveled
-    ==========================================================================================
-    The distance a vehicle travels can be calculated as follows:
-
-    distance = speed * time
-
-    For example, if a train travels 40 miles per hour for 3 hours, the distance traveled is
-    120 miles.
-
-    Write a program that asks the user for the speed of a vehicle (in miles per hour) and how
-    many hours it has traveled. The program should then use a loop to display the distance the
-    vehicle has traveled for each hour of that time period. Here is an example of the output:
-
-    What is the speed of the vehicle in mph? 40
-    How many hours has it traveled? 3
-
-		Hour	Distance Traveled			Kilometers
-	-----------------------------------------------------------
-    	1      	 40 miles  				64.3738 kilometers
-    	2      	 80 miles  				128.748 kilometers
-    	3   	 120 miles  			193.121 kilometers
-
-    Input Validation: Do not accept a negative number for speed and do not accept any
-    value less than 1 for time traveled.
-*******************************************************************************************************/
-
 // Headers
 #include <iostream>
 #include <cstdlib>
@@ -32,22 +5,19 @@
 #include <iomanip>
 using namespace std;
 
-// Global variables
 const double CONVERSION_FACTOR = 0.621371;
 
-// Function declarations
 bool isNegative(int num);
 void pause();
 
 int main()
 {
-	bool status = true;				// Sentinel value for do-while loop as a bool
-	int hours = 0,					// Holds time in hours
-		speed = 0;					// Holds speed in mph
-	double distanceImperial = 0.0,	// Holds distance in imperial
-		   distanceMetric = 0.0;	// Holds distance in metric
-
-	// Use do-while loop to get user's input and validate for speed
+	bool status = true;				
+	int hours = 0,					
+	    speed = 0;					
+	double distanceImperial = 0.0,			
+	       distanceMetric = 0.0;			
+	
 	do
 	{
 		cout << "What is the speed of the vehicle in mph? (As integer): ";
@@ -55,7 +25,6 @@ int main()
 		status = isNegative(speed);
 	} while(status);
 
-	// Use do-while loop to get user's input and validate for hours
 	do
 	{
 		cout << "How many hours has it traveled? (As integer): ";
@@ -63,10 +32,8 @@ int main()
 		status = isNegative(hours);
 	} while(status);
 
-	// Set the numeric output formatting.
-    cout << fixed << showpoint << setprecision(2);
+	cout << fixed << showpoint << setprecision(2);
 
-	// Output the table
 	cout << "\nHour\t\tMiles Traveled\t\tKilometers Traveled"
 		 << "\n-------------------------------------------------------------------\n";
 	for (int i = 1; i <= hours; i++)
@@ -81,10 +48,9 @@ int main()
 	}
 
 	pause();
-    return 0;
+    	return 0;
 }
 
-// Validation of number
 bool isNegative(int num)
 {
 	if (num < 0)
@@ -96,7 +62,6 @@ bool isNegative(int num)
 	return false;
 }
 
-// A non-system dependent method is below
 void pause()
 {
 	cout << "\n\nPress any key to continue....";
