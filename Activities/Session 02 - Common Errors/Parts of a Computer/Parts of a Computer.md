06edf1b4ee114847c21c66faeed7c6f06c557cba
nabler <nabler@Nabler.localdomain>
2018-02-04 23:12:54 -0600

    temp

--- Activities/Session 02 - Common Errors/Parts of a Computer/Parts of a Computer.md
+++ Activities/Session 02 - Common Errors/Parts of a Computer/Parts of a Computer.md
@@ -15,12 +15,14 @@ Choices:
 9. Mouse
 10. Central Processing Unit (CPU)
 11. Overflow (Input/Output Error)
+-->
 
- #![Solution](/Resources/Pictures/toilet_tech_solution.gif)
-<d1>
+<!-- ![Solution](/Resources/Pictures/toilet_tech_solution.gif) -->
+
+<!-- <d1>
   <img id="Solution" style="display:none" src="/Resources/Pictures/toilet_tech_solution.gif" alt="Solution">
   <button type="button" onclick="document.getElementById('Solution').style.display='block'">Solution</button>
-</d1> -->
+</d1>  -->
 
 
 <!DOCTYPE html>
@@ -28,8 +30,6 @@ Choices:
 <body>
   <h1> Parts of a Computer</h1>
   <img src="/home/nabler/PROJECTS/Supplement-Instructions-For-COSC-1436/Resources/Pictures/toilet_tech.gif" alt="Problem">
-
-  <!-- <div style="background-color:black;color:white;padding:20px;"> -->
     <h2>Match the terms to the parts of a Computer</h2>
     <h3>Choices:</h3>
     <ol type="1">
@@ -45,9 +45,19 @@ Choices:
       <li>Central Processing Unit (CPU)</li>
       <li>Overflow (Input/Output Error)</li>
     </ol>
-  <!-- </div> -->
-  <!-- ![Solution](/Resources/Pictures/toilet_tech_solution.gif) -->
-  <img id="Solution" style="display:none" src="/home/nabler/PROJECTS/Supplement-Instructions-For-COSC-1436/Resources/Pictures/toilet_tech_solution.gif" alt="Solution">
+    Choices:
+    1. Input
+    2. Output
+    3. User Interface
+    4. Application Software
+    5. Floppy Disk
+    6. Main Storage
+    7. Debugger
+    8. Backup system
+    9. Mouse
+    10. Central Processing Unit (CPU)
+    11. Overflow (Input/Output Error) 
+  <img id="Solution" style="display:none" src="/Resources/Pictures/toilet_tech_solution.gif" alt="Solution">
   <br><button type="button" onclick="document.getElementById('Solution').style.display='block'">Solution</button>
 
 </body>
--- Activities/Session 02 - Common Errors/Sabotage/Sabotage.cpp
+++ Activities/Session 02 - Common Errors/Sabotage/Sabotage.cpp
@@ -1,6 +1,6 @@
-// This program calculates the amount of pay that will
-// be contributed to a retirement plan if 5%, 7%, or 10%
-// of monthly pay is withheld.
+// This program calculates the amount of chariable contribution that will
+// be contributed to a charity if 5%, 7%, or 10%
+// of monthly chariable contribution is withheld.
 #include <iostream>
 using namespace std;
 
@@ -9,23 +9,21 @@ void pause();
 
 int main()
 {
-    // Variables to hold the monthly pay and the
-    // amount of contribution.
-    double monthlyPay = 6000.0, contribution;
+    double monthlyChariableContribution = 6000.0,   // Variables to hold the monthly chariable contribution and the
+           currentContribution;                     // amount of currentContribution.
 
-    // Calculate and display a 5% contribution.
-    contribution = monthlyPay * 0.05;
-    cout << "5 percent is $" << contribution
+    // Calculate and display a 5% currentContribution.
+    currentContribution = monthlyChariableContribution * 0.05;
+    cout << "5 percent is $" << currentContribution << " per month.\n";
+
+    // Calculate and display a 7% currentContribution.
+    currentContribution = monthlyChariableContribution * 0.07;
+    cout << "7 percent is $" << currentContribution
          << " per month.\n";
 
-    // Calculate and display a 7% contribution.
-    contribution = monthlyPay * 0.07;
-    cout << "7 percent is $" << contribution
-         << " per month.\n";
-
-    // Calculate and display a 10% contribution.
-    contribution = monthlyPay * 0.1;
-    cout << "10 percent is $" << contribution
+    // Calculate and display a 10% currentContribution.
+    currentContribution = monthlyChariableContribution * 0.1;
+    cout << "10 percent is $" << currentContribution
          << " per month.\n";
 
     pause(); // pauses the console
@@ -35,7 +33,7 @@ int main()
 // A non-system dependent method to pause the console
 void pause()
 {
-  cin.clear();
+    cin.clear();
 	cout << "\n\nPress enter to continue....";
 	cin.get();
 }
