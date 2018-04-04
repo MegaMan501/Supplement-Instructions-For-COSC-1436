3642e6bce2d6098dbbfdb287e41bddb069c249f7
Mohamed Rahaman <mohamed.rahaman@my.tccd.edu>
2018-04-04 16:12:23 -0500

    Create fizzBuzz.cpp

---
+++ Resources/2018SP/fizzBuzz.cpp
@@ -0,0 +1,34 @@
+// Write a program that prints the numbers from 1 to 100 
+// and for multiples of '3' print "Fizz" instead of the 
+// number and for the multiples of '5' print "Buzz".
+#include <iostream>
+
+// Function Declaration
+void pause(); 
+
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz");		// std::cout << "FizzBuzz";
+		else if ((i % 3) == 0)
+			printf("Fizz");			// std::cout << "Fizz";
+		else if ((i % 5) == 0)
+			printf("Buzz");			// std::cout << "Buzz";
+		else
+			printf("%d", i);			// std::cout << i;
+		std::cout << std::endl; 
+	}
+
+	pause(); 
+	return 0; 
+}
+
+// pauses the console
+void pause()
+{
+	std::cout << "\n\nPress Enter....."; 
+	std::cin.ignore(); 
+	std::cin.get();
+}
